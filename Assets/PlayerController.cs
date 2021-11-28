c606ea3237216107b3d2c66012387ff5816f62ae
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-11-28 16:43:45 +0700

    [Player] script and animate attack

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
 
     public float maxSpeed = 10f;
 
+    public float fireRate = 0.2f;
+
+    public float nextFireRate = 0.0f;
+
     private Rigidbody2D rb2d;
 
     private Animator anim;
@@ -28,13 +32,23 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
         if (Input.GetAxis("Horizontal") < -0.1f)
         {
-            transform.Translate(Vector2.right * speed *  Time.deltaTime);
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 180);
         }
         else if (Input.GetAxis("Horizontal") > 0.1f)
         {
-            transform.Translate(Vector2.right * speed *  Time.deltaTime);
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J) && Time.time > nextFireRate)
+        {
+            nextFireRate = Time.time + fireRate;
+            anim.SetBool("Attack", true);
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
         }
     }
 }
