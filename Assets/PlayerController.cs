517f3c36f48f82a9e95c47b1c7079de6fad932b2
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2021-11-28 16:07:37 +0700

    player Walk animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+
+    private Animator anim;
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("Grounded", true);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            // transform.Translate(Vector2.right);
+            // transform.eulerAngles = new Vector2(0, 180);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            // transform.Translate(Vector2.right);
+            // transform.eulerAngles = new Vector2(0, 0);
+        }
     }
 }
